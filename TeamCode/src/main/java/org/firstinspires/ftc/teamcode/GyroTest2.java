344813e5083fb6e77b45768d8bd3079ee521817f
FTC4924 <nrrobotics@gmail.com>
2018-11-06 11:40:50 -0500

    Might Need to Fine Tune Turning, But is Able to Hit -Ayca

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroTest2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroTest2.java
@@ -1,27 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
-
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@Autonomous(name="GyroTest", group="Pushbot")
+@Autonomous(name="GyroTest2", group="Pushbot")
 
-public class GyroTest extends LinearOpMode {
+public class GyroTest2 extends LinearOpMode {
 
 
     ColorSensor sensorColorRight;
@@ -78,7 +76,6 @@ public class GyroTest extends LinearOpMode {
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.REVERSE);
-
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
         parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
@@ -105,7 +102,7 @@ public class GyroTest extends LinearOpMode {
         telemetry.update();
 
         waitForStart();
-        turnToPosition(.5, -10);
+        turnToPosition(.5, 10);
         encoderDrive(DRIVE_SPEED, 12, 12, 5);
 
 
